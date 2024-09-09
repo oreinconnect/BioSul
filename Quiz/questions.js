ff91320527e6f4a09244bf5752214690c96a51bf
Julio Cesar <juliocesaromartins@gmail.com>
2024-09-09 08:27:13 -0300

    melhorando o quiz

--- Quiz/questions.js
+++ Quiz/questions.js
@@ -2,10 +2,10 @@ export default[
     {
         question: "Qual bioma da América do Sul se estende ao longo da costa leste e é um dos mais ameaçados pela urbanização?",
         answers: [
-            { option: "Amazônia", correct: false },
-            { option: "Pampas", correct: false },
-            { option: "Mata Atlântica", correct: true },
-            { option: "Pantanal", correct: false }
+            { option: "A) Amazônia", correct: false },
+            { option: "B) Pampas", correct: false },
+            { option: "C) Mata Atlântica", correct: true },
+            { option: "D) Pantanal", correct: false }
         ]
     },
 
@@ -13,10 +13,10 @@ export default[
     {
         question: "Qual é o maior bioma da América do Sul?",
         answers: [
-            { option: "Pantanal", correct: false },
-            { option: "Mata Atlântica", correct: false },
-            { option: "Amazônia", correct: true },
-            { option: "Cerrado", correct: false }
+            { option: "A) Pantanal", correct: false },
+            { option: "B) Mata Atlântica", correct: false },
+            { option: "C) Amazônia", correct: true },
+            { option: "D) Cerrado", correct: false }
         ]
     },
 
@@ -24,10 +24,10 @@ export default[
     {
         question: "Em qual bioma da América do Sul encontramos uma combinação única de savanas, florestas secas e formações rochosas, contribuindo para uma das maiores taxas de endemismo de espécies vegetais?",
         answers: [
-            { option: "Pantanal", correct: false },
-            { option: "Cerrado", correct: true },
-            { option: "Pampas", correct: false },
-            { option: "Caatinga", correct: false }
+            { option: "A) Pantanal", correct: false },
+            { option: "B) Cerrado", correct: true },
+            { option: "C) Pampas", correct: false },
+            { option: "D) Caatinga", correct: false }
         ]
     },
 
@@ -35,20 +35,20 @@ export default[
     {
         question: "Qual bioma da América do Sul é responsável por abrigar o maior número de espécies de plantas adaptadas a solos ácidos e ricos em alumínio?",
         answers: [
-            { option: "Caatinga", correct: false },
-            { option: "Pantanal", correct: false },
-            { option: "Mata Atlântica", correct: false },
-            { option: "Cerrado", correct: true }
+            { option: "A) Caatinga", correct: false },
+            { option: "B) Pantanal", correct: false },
+            { option: "C) Mata Atlântica", correct: false },
+            { option: "D) Cerrado", correct: true }
         ]
     },
 
     {
         question: "Qual bioma da América do Sul, apesar de estar em uma região de clima tropical, apresenta temperaturas médias anuais relativamente baixas devido à sua altitude elevada?",
         answers: [
-            { option: "Andes Tropicais", correct: true },
-            { option: "Mata Atlântica", correct: false },
-            { option: "Cerrado", correct: false },
-            { option: "Pampas", correct: false }
+            { option: "A) Andes Tropicais", correct: true },
+            { option: "B) Mata Atlântica", correct: false },
+            { option: "C) Cerrado", correct: false },
+            { option: "D) Pampas", correct: false }
         ]
     }
 ]
