df68a3676b2a15096dd7e083a1d667c443328f55
Julio Cesar <juliocesaromartins@gmail.com>
2024-09-15 22:47:17 -0300

    quizteste

---
+++ QuizTeste/questions.js
@@ -0,0 +1,54 @@
+export default[
+    {
+        question: "1. Qual bioma da América do Sul se estende ao longo da costa leste e é um dos mais ameaçados pela urbanização?",
+        answers: [
+            { option: "A) Amazônia", correct: false },
+            { option: "B) Pampas", correct: false },
+            { option: "C) Mata Atlântica", correct: true },
+            { option: "D) Pantanal", correct: false }
+        ]
+    },
+
+    
+    {
+        question: "2. Qual é o maior bioma da América do Sul?",
+        answers: [
+            { option: "A) Pantanal", correct: false },
+            { option: "B) Mata Atlântica", correct: false },
+            { option: "C) Amazônia", correct: true },
+            { option: "D) Cerrado", correct: false }
+        ]
+    },
+
+
+    {
+        question: "3. Em qual bioma da América do Sul encontramos uma combinação única de savanas, florestas secas e formações rochosas, contribuindo para uma das maiores taxas de endemismo de espécies vegetais?",
+        answers: [
+            { option: "A) Pantanal", correct: false },
+            { option: "B) Cerrado", correct: true },
+            { option: "C) Pampas", correct: false },
+            { option: "D) Caatinga", correct: false }
+        ]
+    },
+
+
+    {
+        question: "4. Qual bioma da América do Sul é responsável por abrigar o maior número de espécies de plantas adaptadas a solos ácidos e ricos em alumínio?",
+        answers: [
+            { option: "A) Caatinga", correct: false },
+            { option: "B) Pantanal", correct: false },
+            { option: "C) Mata Atlântica", correct: false },
+            { option: "D) Cerrado", correct: true }
+        ]
+    },
+
+    {
+        question: "5. Qual bioma da América do Sul, apesar de estar em uma região de clima tropical, apresenta temperaturas médias anuais relativamente baixas devido à sua altitude elevada?",
+        answers: [
+            { option: "A) Andes Tropicais", correct: true },
+            { option: "B) Mata Atlântica", correct: false },
+            { option: "C) Cerrado", correct: false },
+            { option: "D) Pampas", correct: false }
+        ]
+    }
+]
